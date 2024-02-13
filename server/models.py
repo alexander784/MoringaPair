5e9798f973e5d4dcadf895ebb2f0d63126aabab7
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-13 16:41:57 +0300

    Validate full_name, username, email

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from config import db
+from sqlalchemy.orm import validates
 
 class User(db.Model):
     __tablename__="users"
@@ -8,6 +9,42 @@ class User(db.Model):
     username=db.Column(db.String, nullable=False, unique=True)
     email=db.Column(db.String, nullable=False, unique=True)
     _password_hash=db.Column(db.String, nullable=False)
+    
+    @validates("full_name")
+    def validate_full_name(self, key, full_name):
+        if not full_name:
+            raise ValueError("Full name required")
+        return full_name
+    
+    @validates("username")
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username required")
+        else:
+            if User.query.filter_by(username=username).first():
+                raise ValueError("Username already exists")
+            return username
+        
+    # john.doe@moringaschool...regex
+    @validates("email")
+    def validate_email(self, key, email):
+        if not email:
+            raise ValueError("Email required")
+        else:
+            if User.query.filter_by(email=email).first():
+                raise ValueError("Email already exists")
+            else:
+                import re
+                pattern=r"[a-z]*.[a-z]*@moringaschool"
+                regex=re.compile(pattern)
+                
+                # search, match, findall
+                if not regex.fullmatch(email):
+                    raise ValueError("Invalid email format")
+                return email
+                
+    
+        
     
     def __repr__(self):
         return f"User {self.full_name} {self.username} {self.email}"
