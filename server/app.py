54b19387dbae9d10bed13e7174d70ef9ec4e0f44
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-13 18:55:08 +0300

    Implement jwt error handling == handle expired, invalid, missing tokens

--- server/app.py
+++ server/app.py
@@ -1,11 +1,29 @@
 from config import app
 from controllers.auth_controllers import auth_bp
 from controllers.user_controllers import user_bp
+from config import jwt
+from flask import make_response, jsonify
 
 
 # register blueprints
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(user_bp, url_prefix="/api")
+
+# JWT STUFF
+# !jwt error handling == invalid, expired, missing token
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return make_response(jsonify({"message":"Token has expired", "error":"token_expired"}), 400)
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return make_response(jsonify({"message":"Signature verification failed", "error":"token_invalid"}), 400)
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return make_response(jsonify({"message":"Token is required", "error":"token_missing"}), 400)
+
+
 
 
 if __name__ == "__main__":
