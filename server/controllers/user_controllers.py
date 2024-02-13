04114dd435f139b6f6a2917b55f71830dae09518
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-13 18:29:49 +0300

    Implement Users, UserById resources

--- server/controllers/user_controllers.py
+++ server/controllers/user_controllers.py
@@ -1,24 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify, request
 from flask_restful import Api, Resource
+from models import User
+from config import db
 
 user_bp=Blueprint("user_bp", __name__)
 api=Api(user_bp)
 
 class Users(Resource):
     def get(self):
-        pass
+        # users list comprehension
+        users_lc=[user.to_dict() for user in User.query.all()]
+        
+        return make_response(jsonify({"users":users_lc}), 200)
 
 class UserById(Resource):
     def get(self, user_id):
-        pass
+        user=User.query.filter_by(id=user_id).first()
+        
+        if not user:
+            return make_response(jsonify({"error":"User not found"}), 401)
+        
+        return make_response(jsonify({"user":user.to_dict()}), 200)
     
     def patch(self, user_id):
-        pass
+        data=request.get_json()
+        user=User.query.filter_by(id=user_id).first()
+        
+        if not user:
+            return make_response(jsonify({"error":"User not found"}), 401)
+        
+        for attr in data:
+            setattr(user, attr, data.get(attr))
+            
+        db.session.add(user)
+        db.session.commit()
+        
+        return make_response(jsonify({"user":user.to_dict()}), 200)
     
     def delete(self, user_id):
-        pass
+        user=User.query.filter_by(id=user_id).first()
+        
+        if not user:
+            return make_response(jsonify({"error":"User not found"}), 401)
+        
+        db.session.delete(user)
+        db.session.commit()
+        
+        return make_response(jsonify({"message":"User deleted successfully"}), 204)
     
     
 # URLs
 api.add_resource(Users, "/users")
-api.add_resource(UserById, "/users/<int: user_id>")
+api.add_resource(UserById, "/users/<int:user_id>")
