098b5f779ba1df4698c2ce19ec5a17cb4c03323c
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-16 18:32:47 +0300

    Add UserSchema, StudentSchema == aids to avoid hustle of using SerializerMixin

---
+++ server/marshmallow_schemas.py
@@ -0,0 +1,33 @@
+from config import ma
+from models import Student
+from marshmallow import fields
+
+
+class UserSchema(ma.Schema):
+    id = fields.Int(dump_only=True)
+    full_name = fields.Str(required=True)
+    username = fields.Str(required=True, unique=True)
+    email = fields.Email(required=True, unique=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    _password_hash = fields.Str(required=True)
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+class StudentSchema(ma.Schema):
+    class Meta:
+        model = Student
+
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    user_id = fields.Int()
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+
+student_schema = StudentSchema()
+students_schema = StudentSchema(many=True)
