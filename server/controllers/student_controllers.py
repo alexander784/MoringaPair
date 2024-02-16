06107f677b60141cf575114f9e268714e7757e40
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-16 16:48:27 +0300

    Protect student routes

--- server/controllers/student_controllers.py
+++ server/controllers/student_controllers.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
+from flask_jwt_extended import jwt_required
 
 # student_bp
 student_bp = Blueprint("student_bp", __name__)
@@ -8,20 +9,25 @@ api = Api(student_bp)
 
 # resources
 class Students(Resource):
+    @jwt_required()
     def get(self):
         pass
-
+    
+    @jwt_required()
     def post(self):
         pass
 
 
 class StudentById(Resource):
+    @jwt_required()
     def get(self, student_id):
         pass
 
+    @jwt_required()
     def patch(self, student_id):
         pass
-
+    
+    @jwt_required()
     def delete(self, student_id):
         pass
 
