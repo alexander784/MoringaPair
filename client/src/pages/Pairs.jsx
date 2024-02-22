3955cae1ff413ff80443f0825fc31979244b9a06
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-22 10:39:41 +0300

    Implement Pairs component

--- client/src/pages/Pairs.jsx
+++ client/src/pages/Pairs.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Pairs = () => {
-  return (
-    <div>Pairs</div>
-  )
-}
+  const [pairs, setPairs] = useState([]);
 
-export default Pairs
+  const generateRandomPairs = () => {
+    // fetch API
+    fetch("/api/create_pairs", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.pairs) {
+          setPairs(data.pairs);
+        }
+      })
+      .catch((err) => {
+        console.log("Error in generating random pairs", err);
+      });
+  };
+
+  return (
+    <div className="pairs-container">
+      <button className="btn-gen-pair" onClick={generateRandomPairs}>
+        Generate Pairs
+      </button>
+
+      <div className="cards">
+        {pairs &&
+          pairs.map((pair) => {
+            return <div key={pair.id}>pair</div>;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Pairs;
