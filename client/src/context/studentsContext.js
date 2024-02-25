9160cc3ae782c862f491acc90e4b787ca3ef1b0f
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-25 06:35:38 +0300

    Set up students state mgt. using useReducer

--- client/src/context/studentsContext.js
+++ client/src/context/studentsContext.js
@@ -1,11 +1,65 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 
 // StudentsContext
 const StudentsContext = React.createContext();
 
 // StudentsProvider
 const StudentsProvider = ({ children }) => {
-  return <StudentsContext.Provider>{children}</StudentsContext.Provider>;
+  // initialStudentsState
+  const initialStudentsState = {
+    loading: true,
+    students: [],
+    error: "",
+  };
+
+  //   studentsReducer
+  const studentsReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return {
+          ...state,
+          loading: true,
+          students: [],
+          error: "",
+        };
+
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          students: action.payload,
+          error: "",
+        };
+
+      case "FETCH_FAILURE":
+        return {
+          ...state,
+          loading: false,
+          students: [],
+          error: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  //   useReducer
+  const [studentsState, dispatchForStudents] = useReducer(
+    studentsReducer,
+    initialStudentsState
+  );
+
+  return (
+    <StudentsContext.Provider
+      values={{
+        studentsState,
+        dispatchForStudents,
+      }}
+    >
+      {children}
+    </StudentsContext.Provider>
+  );
 };
 
 // useGloablStudentsContext => can be accessed globally
