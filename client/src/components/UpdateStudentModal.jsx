4e0e487d9457361fea2a3d8480d886dcac06a81f
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-26 11:03:29 +0300

    Add Student ID form-control

--- client/src/components/UpdateStudentModal.jsx
+++ client/src/components/UpdateStudentModal.jsx
@@ -15,11 +15,13 @@ function UpdateStudentModal({ showUpdateModal, handleCloseUpdateModal }) {
 
   const formik = useFormik({
     initialValues: {
+      studentId:"",
       name: "",
       email: "",
       user_id: "",
     },
     validationSchema: Yup.object({
+      studentId: Yup.string().required("Student ID required"),
       name: Yup.string().required("Name is required"),
       email: Yup.string()
         .required("Email is required")
@@ -28,7 +30,29 @@ function UpdateStudentModal({ showUpdateModal, handleCloseUpdateModal }) {
     }),
     onSubmit: (values, { resetForm }) => {
       console.log("Updated Student", values);
-      resetForm();
+
+      // fetch API
+      fetch(`/api/students/${values.studentId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+        body: JSON.stringify(values),
+      })
+        .then((response) => {
+          if (response.ok) {
+            resetForm();
+            return response.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log("Error in adding student", err);
+        });
     },
   });
 
@@ -41,11 +65,24 @@ function UpdateStudentModal({ showUpdateModal, handleCloseUpdateModal }) {
         <Modal.Body>
           <Form onSubmit={formik.handleSubmit}>
             <Form.Group className="mb-3">
+              <Form.Label>Student ID</Form.Label>
+              <Form.Control
+                type="input"
+                placeholder="Student ID"
+                autoFocus
+                name="studentId"
+                value={formik.values.studentId}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            {formik.touched && formik.errors.studentId ? (
+              <div className="error">{formik.errors.studentId}</div>
+            ) : null}
+            <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="input"
                 placeholder="John Doe"
-                autoFocus
                 name="name"
                 value={formik.values.name}
                 onChange={formik.handleChange}
