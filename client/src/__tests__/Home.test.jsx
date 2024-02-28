dbfb0dfd5f13ae9556922e43b8c686677bd2a36a
Alexander784 <alexandernyaga1999@gmail.com>
2024-02-28 11:09:45 +0300

    Click signup navigate to signup

--- client/src/__tests__/Home.test.jsx
+++ client/src/__tests__/Home.test.jsx
@@ -2,3 +2,12 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Home from '../pages/Home';
 import { MemoryRouter } from 'react-router-dom';
+
+test('renders home component without crashing', () => { 
+    render(
+        <MemoryRouter>
+            <Home />
+        </MemoryRouter>
+    );
+ });
+ 
