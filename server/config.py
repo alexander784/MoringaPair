9cac6c720b633bfed4e2e1be0e9d1de1121ea99b
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-28 13:48:52 +0300

    Change JWT_SECRET_KEY

--- server/config.py
+++ server/config.py
@@ -17,12 +17,11 @@ app = Flask(__name__)
 db = SQLAlchemy()
 
 # app configurations
-app.secret_key = environ.get("SECRET_KEY")
+app.config["SECRET_KEY"]= environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = environ.get(
     "SQLALCHEMY_TRACK_MODIFICATIONS")
 app.config["SQLALCHEMY_ECHO"] = environ.get("SQLALCHEMY_ECHO")
-# app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
 app.json.compact = False
 
 # mail details
@@ -46,6 +45,7 @@ bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 ma = Marshmallow(app)
 
+app.config["JWT_SECRET_KEY"] = "02a138bf67702b193876870e"
 
 # prevent cross-origin issues
 CORS(app)
