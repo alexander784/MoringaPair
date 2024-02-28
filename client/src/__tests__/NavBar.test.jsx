4e3cca4f4767872e2197935acfefff40131b8c92
Alexander784 <alexandernyaga1999@gmail.com>
2024-02-28 10:22:10 +0300

    Import necessary modules

--- client/src/__tests__/NavBar.test.jsx
+++ client/src/__tests__/NavBar.test.jsx
@@ -1,1 +1,5 @@
-
+import React from 'react';
+import "@testing-library/jest-dom";
+import { render, fireEvent } from '@testing-library/react';
+import NavBar from '../components/NavBar';
+import { MemoryRouter } from 'react-router-dom';
