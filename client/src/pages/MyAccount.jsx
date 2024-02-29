ef44b0b281754dc6bdd6fd846dfdfff7b97003c3
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-29 17:03:22 +0300

    Markup and style the profile edit button

--- client/src/pages/MyAccount.jsx
+++ client/src/pages/MyAccount.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalAuthContext } from "../context/authContext";
+import Button from "react-bootstrap/Button";
 
 const MyAccount = () => {
   const { authState } = useGlobalAuthContext();
@@ -10,36 +11,41 @@ const MyAccount = () => {
   return (
     <>
       {authState.currentUser && (
-        <div className="profile">
-          <div className="info">
-            <span>ID</span>
-            <span>{authState.currentUser.id}</span>
+        <>
+          <div className="profile">
+            <div className="info">
+              <span>ID</span>
+              <span>{authState.currentUser.id}</span>
+            </div>
+            <div className="info">
+              <span>Full Name</span>
+              <span>{authState.currentUser.full_name}</span>
+            </div>
+            <div className="info">
+              <span>Username</span>
+              <span>{authState.currentUser.username}</span>
+            </div>
+            <div className="info">
+              <span>Email</span>
+              <span>{authState.currentUser.email}</span>
+            </div>
+            <div className="info">
+              <span>Created At</span>
+              <span>{authState.currentUser.created_at}</span>
+            </div>
+            <div className="info">
+              <span>Updated At</span>
+              {authState.currentUser.updated_at ? (
+                <span>{authState.currentUser.updated_at}</span>
+              ) : (
+                <span>Up to date</span>
+              )}
+            </div>
           </div>
-          <div className="info">
-            <span>Full Name</span>
-            <span>{authState.currentUser.full_name}</span>
+          <div className="account-btns">
+            <Button className="edit-btn">Edit Account</Button>
           </div>
-          <div className="info">
-            <span>Username</span>
-            <span>{authState.currentUser.username}</span>
-          </div>
-          <div className="info">
-            <span>Email</span>
-            <span>{authState.currentUser.email}</span>
-          </div>
-          <div className="info">
-            <span>Created At</span>
-            <span>{authState.currentUser.created_at}</span>
-          </div>
-          <div className="info">
-            <span>Updated At</span>
-            {authState.currentUser.updated_at ? (
-              <span>{authState.currentUser.updated_at}</span>
-            ) : (
-              <span>Up to date</span>
-            )}
-          </div>
-        </div>
+        </>
       )}
     </>
   );
