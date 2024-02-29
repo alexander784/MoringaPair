87579e80016d33b4edfa85ecb827f10a19e17fbc
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-29 16:46:32 +0300

    Markup and style the profile card

--- client/src/pages/MyAccount.jsx
+++ client/src/pages/MyAccount.jsx
@@ -7,10 +7,42 @@ const MyAccount = () => {
   if (authState.currentUser) {
     console.log("currentUser", authState.currentUser);
   }
-  return <div>
-    {authState.currentUser && <div>
-      <h1>{authState.currentUser.full_name}</h1></div>}
-  </div>;
+  return (
+    <>
+      {authState.currentUser && (
+        <div className="profile">
+          <div className="info">
+            <span>ID</span>
+            <span>{authState.currentUser.id}</span>
+          </div>
+          <div className="info">
+            <span>Full Name</span>
+            <span>{authState.currentUser.full_name}</span>
+          </div>
+          <div className="info">
+            <span>Username</span>
+            <span>{authState.currentUser.username}</span>
+          </div>
+          <div className="info">
+            <span>Email</span>
+            <span>{authState.currentUser.email}</span>
+          </div>
+          <div className="info">
+            <span>Created At</span>
+            <span>{authState.currentUser.created_at}</span>
+          </div>
+          <div className="info">
+            <span>Updated At</span>
+            {authState.currentUser.updated_at ? (
+              <span>{authState.currentUser.updated_at}</span>
+            ) : (
+              <span>Up to date</span>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MyAccount;
