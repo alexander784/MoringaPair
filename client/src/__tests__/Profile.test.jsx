796cb0eb87bfb78cbe5557167ac417a493e02875
Alexander784 <alexandernyaga1999@gmail.com>
2024-02-29 18:57:43 +0300

    IMport necessary modules

---
+++ client/src/__tests__/Profile.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import "@testing-library/jest-dom";
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Profile from '../components/Profile';
+
+
