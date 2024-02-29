dad0a089def79d39f31642d705b6fe99e339ef39
githinjisamson1 <githinjisamson148@gmail.com>
2024-03-01 00:42:17 +0300

    Mind css file positioning

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { StudentsProvider } from "./context/studentsContext";
 import { PairsProvider } from "./context/pairsContext";
 import { AuthProvider } from "./context/authContext";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
