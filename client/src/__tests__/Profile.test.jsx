428d518bca74e7e636be91adff8505d65995def8
Alexander784 <alexandernyaga1999@gmail.com>
2024-02-29 19:03:28 +0300

    Render Profile without crashing

--- client/src/__tests__/Profile.test.jsx
+++ client/src/__tests__/Profile.test.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import "@testing-library/jest-dom";
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Profile from '../components/Profile';
 
+describe('Profile component', () => {
+    it('renders without crashing',() => {
+        render(
+            <MemoryRouter>
+                <Profile />
+            </MemoryRouter>
 
+        );
+    })
+ })
